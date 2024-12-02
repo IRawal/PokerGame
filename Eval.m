d6a3ace3b51b54cef625b37b3e05b32f181bcc5f
IRawal <kyanrawal@gmail.com>
2024-12-02 15:23:00 -0800

    7 card eval

--- Eval.m
+++ Eval.m
@@ -6,7 +6,7 @@ classdef Eval
 	    sorted = hand(:, ind);
 	end
 	function flush = isFlush(hand)
-	    flush = all(hand(2, :) == hand(2, 1))
+	    flush = all(hand(2, :) == hand(2, 1));
 	end
 	%function four = isQuad(hand)
 	%    mode_val = string(mode(categorical(hand(1, :))));
@@ -14,7 +14,7 @@ classdef Eval
 	%end
 	function mult = multiplicity(hand)
 	    mode_val = string(mode(categorical(hand(1, :))));
-	    mult = sum(hand(1, :) == mode_val)
+	    mult = sum(hand(1, :) == mode_val);
 	end
 	function straight = isStraight(hand)
 	    num_arr = str2double(hand(1, :));
@@ -22,14 +22,14 @@ classdef Eval
 	    straight = all([m:m+4] == num_arr);
 	end
 	function highest = high(hand)
-	    highest = str2double(hand(1, end)) 
+	    highest = str2double(hand(1, end));
 	end
 	function full = isFull(hand) 
 	    v = hand(1, :);
 	    u = unique(v);
 	    if length(u) ~= 2
 		full = false;
-		return
+		return;
 	    end
 	    full = ((sum(v == u(1)) == 3) && (sum(v == u(2)) == 2)) || ((sum(v == u(1)) == 2) && (sum(v == u(2)) == 3));
 	end
@@ -42,34 +42,41 @@ classdef Eval
 	function rflush = isRflush(hand)
 	    rflush = Eval.isSflush(hand) && Eval.high(hand) == 14;
 	end
-	function winner = evaluate(hand1)
+	function strength = evaluate(hand1)
 	    hand1 = Eval.sortHand(hand1);
 
 	    % rough calculation to meet basic requirments.
-	    winner = 0;
-	    h = Eval.high(hand1)
+	    strength = 0;
+	    h = Eval.high(hand1);
 	    m = Eval.multiplicity(hand1);
 
-	    winner = winner + h;
+	    strength = strength + h;
 
-	    winner = winner + (m == 2) * 15;
+	    strength = strength + (m == 2) * 15;
 	    
-	    winner = winner + (Eval.isTwopair(hand1)) * 15^2;
+	    strength = strength + (Eval.isTwopair(hand1)) * 15^2;
 
-	    winner = winner + (m == 3) * 15^3;
+	    strength = strength + (m == 3) * 15^3;
 
-	    winner = winner + Eval.isStraight(hand1) * 15^4;
+	    strength = strength + Eval.isStraight(hand1) * 15^4;
 
-	    winner = winner + Eval.isFlush(hand1) * 15^5;
+	    strength = strength + Eval.isFlush(hand1) * 15^5;
 
-	    winner = winner + Eval.isFull(hand1) * 15^6;
+	    strength = strength + Eval.isFull(hand1) * 15^6;
 
 	    
-	    winner = winner + (m == 4) * 15^7;
+	    strength = strength + (m == 4) * 15^7;
 
-	    winner = winner + Eval.isSflush(hand1) * 15^8;
+	    strength = strength + Eval.isSflush(hand1) * 15^8;
 
-	    winner = winner + Eval.isRflush(hand1) * 15^9;
+	    strength = strength + Eval.isRflush(hand1) * 15^9;
+	end
+	function strength = eval7(hand)
+	    inds = nchoosek([1:7], 5);
+	    strength = 0;
+	    for i = [1:21]
+		strength = max(strength, Eval.evaluate(hand(:, inds(i, :))));
+	    end 
 	end
     end
 end
